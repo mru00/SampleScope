a9564d8c3fa1ff1cfbe00e05a646bd9512752628
mru <mru@sisyphus.teil.cc>
2012-05-19 12:50:31 +0200

    include license string in source code

--- frontend/Osci/channelcontrol.h
+++ frontend/Osci/channelcontrol.h
@@ -1,3 +1,23 @@
+/*
+
+SampleScope: A humble USB oscilloscope
+Copyright (C) 2012 mru
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef CHANNELCONTROL_H
 #define CHANNELCONTROL_H
 
